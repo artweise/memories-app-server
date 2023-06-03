c9bd142e665f623efa73289631cd5cd6e06699c2
leralys <valerialysko@gmail.com>
2023-06-03 15:02:52 +0300

    feat(swagger): init swagger docs
    see the docs on http://localhost:5005/api-docs/

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-import { config } from "dotenv";
-import mongoose from "mongoose";
-import express from "express";
+import { config } from 'dotenv';
+import mongoose from 'mongoose';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import fs from 'fs';
+import YAML from 'yaml';
 
-import { middlewareConfig } from "./config/index.js";
-import { errorHandler } from "./middleware/errorHandler.js";
-import indexRoutes from "./routes/index.routes.js";
-import authRoutes from "./routes/auth.routes.js";
-import familyRoutes from "./routes/family.routes.js";
-import memoryRoutes from "./routes/memory.routes.js";
+import { middlewareConfig } from './config/index.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import indexRoutes from './routes/index.routes.js';
+import authRoutes from './routes/auth.routes.js';
+import familyRoutes from './routes/family.routes.js';
+import memoryRoutes from './routes/memory.routes.js';
 
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
 config();
 
-// --- DB ---
+// ℹ️ --- DB ---
 // Connects to the database
 // package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
@@ -21,7 +25,7 @@ config();
 // Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/memories";
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/memories';
 
 mongoose
   .connect(MONGO_URI)
@@ -30,23 +34,49 @@ mongoose
     console.log(`Connected to Mongo! Database name: "${dbName}"`);
   })
   .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
+    console.error('Error connecting to mongo: ', err);
   });
 
-// Handles http requests (express is node js framework)
+// ℹ️ --- Swagger docs ---
+
+// Load the Swagger specifications from the YAML files
+const memorySpec = YAML.parse(fs.readFileSync('./swagger/memory.yaml', 'utf8'));
+const testSpec = YAML.parse(fs.readFileSync('./swagger/test.yaml', 'utf8'));
+
+// Combine the specifications
+const combinedSpec = {
+  ...memorySpec,
+  ...testSpec,
+  paths: {
+    ...memorySpec.paths,
+    ...testSpec.paths,
+  },
+  // Add more specifications if needed
+};
+
+// Swagger options
+const options = {
+  swaggerDefinition: combinedSpec,
+  apis: ['./routes/*.js'], // Add your API files here
+};
+
+// Initialize swagger-jsdoc
+const swaggerSpecs = swaggerJsdoc(options);
+
+// ℹ️ Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const app = express();
 
-// This function is getting exported from the config folder. It runs most pieces of middleware
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 // (logger, body-parser etc)
 middlewareConfig(app);
 
-// --- ROUTES ---
-
-app.use("/api", indexRoutes);
-app.use("/auth", authRoutes);
-app.use("/api", familyRoutes);
-app.use("/api", memoryRoutes);
+// ℹ️ --- ROUTES ---
+app.use('/api', indexRoutes);
+app.use('/auth', authRoutes);
+app.use('/api', familyRoutes);
+app.use('/api', memoryRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 errorHandler(app);
